d2a2f0583f056e517c8f511ccc8c2b77f31169a1
Vijay <thoratvijay52@gmail.com>
2024-08-24 19:55:21 +0530

    intial commit

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bot-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent  implements OnInit{
+
+  name: string = 'Vijay';
+
+  ngOnInit() : void {
+ 
+  }
+
+}
